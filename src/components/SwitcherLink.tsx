9260c1bece922434a909ff8f5af9155de0e5f85b
Precontation <77246128+Precontation@users.noreply.github.com>
2026-01-19 04:17:01 -0800

    hover makes underline under switcher panel

--- src/components/SwitcherLink.tsx
+++ src/components/SwitcherLink.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function SwitcherLink({
   to,
@@ -7,10 +10,15 @@ export default function SwitcherLink({
   to: string;
   name: string;
 }) {
+  const [isMouseOver, setIsMouseOver] = useState<boolean>(false);
   return (
-    <nav className="self-center">
+    <nav
+      className="self-center flex flex-col"
+      onMouseEnter={() => setIsMouseOver(true)}
+      onMouseLeave={() => setIsMouseOver(false)}
+    >
       <Link href={`/${to}`}>{name}</Link>
-      <span></span>
+      {isMouseOver && <span className="bg-secondary min-w-1 min-h-0.5"></span>}
     </nav>
   );
 }
