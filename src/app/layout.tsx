9f406d0fda18251c6e08b0efbb6b39fb3c60419d
Precontation <77246128+Precontation@users.noreply.github.com>
2026-01-19 22:16:21 -0800

    re-Give metadata to background, fix stupid system that i made because of an old error, and set color thing in seperate function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,21 @@
-"use client";
+import SetColor from "@/components/SetColor";
 import SwitcherLink from "@/components/SwitcherLink";
-import { useEffect, useRef } from "react";
+import { Metadata } from "next";
 import "./globals.css";
 
-// export const metadata: Metadata = {
-//   title: "Precontation",
-//   description: "Epic generic website! (wow)",
-// };
+export const metadata: Metadata = {
+  title: "Precontation",
+  description: "Epic generic website! (wow)",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // Stackoverflow, you may be pretty much dead, but you aren't dead in my heart!
-  // Modified from https://stackoverflow.com/questions/1484506/random-color-generator
-  function getRandomColor() {
-    const letters = "89ABCDEF89ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
-  const rootRef = useRef<HTMLHtmlElement>(null);
-
-  useEffect(() => {
-    rootRef.current?.style.setProperty("--secondary", getRandomColor());
-  }, []);
-
   return (
-    <html lang="en" ref={rootRef}>
+    <html lang="en">
+      <SetColor />
       <body className="antialiased flex flex-col min-h-screen">
         <header className="backdrop-blur-3xl border-b sticky top-0 min-w-full z-100">
           <nav className="flex gap-10 h-10 justify-center">
