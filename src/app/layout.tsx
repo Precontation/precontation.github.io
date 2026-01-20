ea0c60b062cb98956ab415c59a0c4223bea7091b
SupKittyMeow <supkittymeow@gmail.com>
2026-01-20 13:30:03 -0800

    sliightly better splash text (still bad). hopefully now shows the name instead of the website url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import SetColor from "@/components/SetColor";
 import SwitcherLink from "@/components/SwitcherLink";
 import { Metadata } from "next";
-import { CSSProperties } from "react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,22 +13,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  function getRandomColor() {
-    const letters = "89ABCDEF89ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      // eslint-disable-next-line react-hooks/purity
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
   return (
-    <html
-      lang="en"
-      style={{ "--secondary": getRandomColor() } as CSSProperties}
-    >
+    <html lang="en">
       <body className="antialiased flex flex-col min-h-screen">
+        <SetColor />
         <header className="backdrop-blur-3xl border-b sticky top-0 min-w-full z-100">
           <nav className="flex gap-10 h-10 justify-center">
             <SwitcherLink to="" name="Home"></SwitcherLink>
