ea75e3e000da6b76be2a1e25915ed787ff6e42cc
Precontation <77246128+Precontation@users.noreply.github.com>
2026-01-28 19:45:19 +0000

    add todo not that special but yeah

--- src/components/SetColor.tsx
+++ src/components/SetColor.tsx
@@ -6,7 +6,7 @@ export default function SetColor() {
   // Stackoverflow, you may be pretty much dead, but you aren't dead in my heart!
   // Modified from https://stackoverflow.com/questions/1484506/random-color-generator
   function getRandomColor() {
-    const letters = "89ABCDEF89ABCDEF";
+    const letters = "89ABCDEF89ABCDEF"; // TODO: if in light mode, do opposite
     let color = "#";
     for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
